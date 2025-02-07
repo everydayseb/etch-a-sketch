ecb27613ca0c50e92eb42fb1b273a3ced9267f59
Seb <everydayseb@pm.me>
2025-02-07 18:03:11 +0000

    Add automatic adjustment to cell size depending on number of cells

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,19 @@
 const container = document.querySelector("#container");
 
 const createGrid = function (container, rows, cols) {
-    
+    const cellSize = 640 / rows;
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
             const div = document.createElement("div");
             div.setAttribute("class", "cell");
+            div.style.width = `${cellSize + "px"}`;
+            div.style.height = `${cellSize + "px"}`;
             container.appendChild(div);
         }
     }
 }
 
-createGrid(container, 10, 10);
+createGrid(container, 64, 64);
 
 const cells = container.querySelectorAll("div");
 
