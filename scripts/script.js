e21d46baf9e737ccbc93e533859888705c612451
Seb <everydayseb@pm.me>
2025-02-07 17:39:30 +0000

    Add cells change colour on hover

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,10 @@ const createGrid = function (container, rows, cols) {
 
 createGrid(container, 16, 16);
 
+const cells = container.querySelectorAll("div");
+
+cells.forEach((cell) => {
+    cell.addEventListener("mouseover", () => {
+        cell.style.backgroundColor = "#363636";
+    });
+});
